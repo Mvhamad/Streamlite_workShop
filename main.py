5109cb2f840627f4c15f9faf676a8a7d701bf082
Mvhamad <bamohamed961@gmail.com>
2024-07-30 20:45:32 +0000

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title("Bienvenue sur mon atelier")
 st.text("Aujourd'hui nous allons apprendre comment deployer un modele sur streamlit\nMerci de votre attention")
+st.button('Button')
